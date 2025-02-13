c552c08e693d82f75cf0cd657a76ecda4be4b9ca
Hbartlett5 <hbcollege5@gmail.com>
2025-02-13 09:18:10 -0500

    Updated

--- Exceptional_Weather_Forecast.py
+++ Exceptional_Weather_Forecast.py
@@ -19,15 +19,17 @@ def temp_conversion(temperature):
         temp_multiply = temp_subtract * 5
         temp_celsius = temp_multiply / 9
         celsius = round(temp_celsius, 2)
-    except ZeroDivisionError:
-        pass
+    except TypeError:
+        print("An unexpected TypeError has occurred.")
+
+    # Task 3: User Experience
+
     else:
         print(f"{temperature} degrees Fahrenheit is {celsius} degrees Celsius.")
+    
+    # Task 4: Finally
+
     finally:
-        print("Thank you for using the Temperature Converter.")
+        print("Thank you for using the Weather Forecast Converter.")
 
 temp_conversion(temperature)
-
-# Task 3: User Experience
-
-# Task 4: Finally
